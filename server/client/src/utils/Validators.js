60e6c4cc6fb06cf79bb5e38fe31a56c00ad2f067
Akshintala Manikanta Surendra kumar <87278812+surendra-1390@users.noreply.github.com>
2022-09-12 01:40:29 +0530

    changed directory

--- server/client/src/utils/Validators.js
+++
@@ -1,9 +0,0 @@
-import { EMAIL_REGEX } from "../constants/constants"
-
-export const isEmailValid = email => {
-    return EMAIL_REGEX.test(email);
-}
-
-export const isEmailInValid = email => {
-    return !isEmailValid(email);
-}
